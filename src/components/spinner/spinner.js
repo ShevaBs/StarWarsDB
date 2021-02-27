dc8462324b74041c6bc746b4bacca5871103c374
Bogdan <bheva95@gmail.com>
2021-02-27 11:46:23 +0200

    add spinner to random-planet component and initialize loading logic

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-anxenouwllt">
+      <div class="ldio-uvmsea7pyfj">
+        <div></div>
+          <div></div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Spinner;
